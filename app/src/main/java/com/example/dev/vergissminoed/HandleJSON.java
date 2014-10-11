5ee1f178de3fddcac15073b6cc179e66b510ce86
oilst <ueli.peter@gmail.com>
2014-10-11 18:57:24 +0200

    fetch data from server

---
+++ app/src/main/java/com/example/dev/vergissminoed/HandleJSON.java
@@ -0,0 +1,98 @@
+package com.example.dev.vergissminoed;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.text.SimpleDateFormat;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import android.annotation.SuppressLint;
+
+public class HandleJSON {
+
+    private HashMap<Date, ArrayList<String>> data;
+    private String urlString = null;
+
+    public volatile boolean parsingComplete = true;
+
+    public HandleJSON(String url) {
+        this.urlString = url;
+    }
+
+    public HashMap<Date, ArrayList<String>> getData() {
+        return data;
+    }
+
+    @SuppressLint("NewApi")
+    public void readAndParseJSON(String in) {
+        try {
+            JSONObject reader = new JSONObject(in);
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+
+            Iterator<String> it = reader.keys();
+
+            data = new HashMap<Date, ArrayList<String>>();
+
+            for (; it.hasNext(); ) {
+                String s = it.next();
+                Date d = dateFormat.parse(s);
+                ArrayList<String> al = new ArrayList<String>();
+                JSONArray jsonarray = reader.getJSONArray(s);
+                for (int i = 0; i < jsonarray.length(); i++) {
+                    al.add(i, jsonarray.getString(i));
+                }
+                data.put(d, al);
+            }
+
+            parsingComplete = false;
+
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    public void fetchJSON() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(urlString);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setReadTimeout(10000 /* milliseconds */);
+                    conn.setConnectTimeout(15000 /* milliseconds */);
+                    conn.setRequestMethod("GET");
+                    conn.setDoInput(true);
+                    // Starts the query
+                    conn.connect();
+                    InputStream stream = conn.getInputStream();
+
+                    String da = convertStreamToString(stream);
+
+                    readAndParseJSON(da);
+                    stream.close();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+    }
+
+    static String convertStreamToString(java.io.InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+    }
+
+}
--- app/src/main/java/com/example/dev/vergissminoed/ShowList.java
+++ app/src/main/java/com/example/dev/vergissminoed/ShowList.java
@@ -7,52 +7,77 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
 
 public class ShowList extends ListActivity {
     ArrayAdapter<String> mAdapter;
 
+    private String url1 = "http://vergissminoed.appspot.com/";
+    private HandleJSON obj;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_list);
+        try{
+            obj = new HandleJSON(url1);
+            obj.fetchJSON();
+            while(obj.parsingComplete);
+            //Date today = new Date();
 
-        // Set up ListView example
-        ArrayList<String> items = new ArrayList<String>(
-                Arrays.asList("Milk", "Toilet paper", "Yoghurt", "Nespresso"));
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            Date d = dateFormat.parse("2014-10-10");
 
-        mAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,
-                android.R.id.text1,
-                items);
-        setListAdapter(mAdapter);
 
-        ListView listView = getListView();
-        // Create a ListView-specific touch listener. ListViews are given special treatment because
-        // by default they handle touches for their list items... i.e. they're in charge of drawing
-        // the pressed state (the list selector), handling list item clicks, etc.
-        SwipeDismissListViewTouchListener touchListener =
-                new SwipeDismissListViewTouchListener(
-                        listView,
-                        new SwipeDismissListViewTouchListener.DismissCallbacks() {
-                            @Override
-                            public boolean canDismiss(int position) {
-                                return true;
-                            }
 
-                            @Override
-                            public void onDismiss(ListView listView, int[] reverseSortedPositions) {
-                                for (int position : reverseSortedPositions) {
-                                    mAdapter.remove(mAdapter.getItem(position));
+            ArrayList<String> items = obj.getData().get(d);
+
+            //ArrayList<String> items  = new ArrayList<String>(
+            //       Arrays.asList("Milk", "Toilet paper", "Yoghurt", "Nespresso"));
+
+            mAdapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1,
+                    android.R.id.text1,
+                    items);
+            setListAdapter(mAdapter);
+
+            ListView listView = getListView();
+            // Create a ListView-specific touch listener. ListViews are given special treatment because
+            // by default they handle touches for their list items... i.e. they're in charge of drawing
+            // the pressed state (the list selector), handling list item clicks, etc.
+            SwipeDismissListViewTouchListener touchListener =
+                    new SwipeDismissListViewTouchListener(
+                            listView,
+                            new SwipeDismissListViewTouchListener.DismissCallbacks() {
+                                @Override
+                                public boolean canDismiss(int position) {
+                                    return true;
                                 }
-                                mAdapter.notifyDataSetChanged();
-                            }
-                        });
-        listView.setOnTouchListener(touchListener);
-        // Setting this scroll listener is required to ensure that during ListView scrolling,
-        // we don't look for swipes.
-        listView.setOnScrollListener(touchListener.makeScrollListener());
+
+                                @Override
+                                public void onDismiss(ListView listView, int[] reverseSortedPositions) {
+                                    for (int position : reverseSortedPositions) {
+                                        mAdapter.remove(mAdapter.getItem(position));
+                                    }
+                                    mAdapter.notifyDataSetChanged();
+                                }
+                            });
+            listView.setOnTouchListener(touchListener);
+            // Setting this scroll listener is required to ensure that during ListView scrolling,
+            // we don't look for swipes.
+            listView.setOnScrollListener(touchListener.makeScrollListener());
+        }catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     @Override
